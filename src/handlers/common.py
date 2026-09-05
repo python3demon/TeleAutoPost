8736ab4f93d0144a237a284150f0a3b00e9132b9
Sith <227094824+python3demon@users.noreply.github.com>
2026-09-05 13:01:03 +0300

    refactor: подключена реплай-клавиатура и обновлен /help, closes #2

--- src/handlers/common.py
+++ src/handlers/common.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 
 from config import config_user
 from states.bot_states import Registration
+from keyboards.reply import kb_start_reply
 
 router = Router()
 
@@ -15,7 +16,8 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
             f"🤖 Привет, {html.bold(message.from_user.full_name)}!\n\n"
             f"Добро пожаловать в {html.code('TeleAutoPost')} — бот для автоматической "
             f"отправки и планирования постов.\n\n"
-            f"Используйте команду /help, чтобы увидеть список доступных функций."
+            f"Используйте команду /help, чтобы увидеть список доступных функций.",
+            reply_markup=kb_start_reply
         )
     else:
         config_user["id"] = message.from_user.id
@@ -33,5 +35,5 @@ async def command_help_handler(message: Message) -> None:
         f"Доступные команды на данный момент:\n"
         f"/start — Запустить бота и получить приветственное сообщение\n"
         f"/help — Показать эту справку по командам.\n\n"
-        f"Для отправки поста на канал просто введите текст.",
+        f"Для отправки поста на канал введите {html.code("Создать пост")}.",
     )
