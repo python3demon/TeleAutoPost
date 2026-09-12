3e2adffd70d6c3e66f402edeffe1a4b6c7854905
Sith <227094824+python3demon@users.noreply.github.com>
2026-09-12 20:59:53 +0300

    feat: поддержка фото при отправке черновика #10

--- src/handlers/drafts.py
+++ src/handlers/drafts.py
@@ -2,6 +2,7 @@ from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from config import config_user
 from keyboards.inline import build_list_draft
+from aiogram.utils.media_group import MediaGroupBuilder
 
 router = Router()
 
@@ -16,5 +17,13 @@ async def get_my_draft(message: Message):
 async def cat_draft(callback: CallbackQuery):
     num_draft = callback.data.split("_")[-1]
     post_text = config_user["drafts"][num_draft]["post"]
-
-    await callback.message.edit_text(f"{post_text}\n\nВаш черновик*", reply_markup=None)
+    group_photo = config_user["drafts"][num_draft]["group_photo"]
+    
+    if not group_photo:
+        await callback.message.edit_text(f"{post_text}\n\nВаш черновик*", reply_markup=None)
+    else:
+        builder = MediaGroupBuilder(caption=post_text)
+        for photo_id in group_photo:
+            builder.add_photo(media=photo_id)
+        
+        await callback.message.answer_media_group(media=builder.build())
