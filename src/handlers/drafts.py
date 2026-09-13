92a25707f0f11a06e9d1efa55de0dea696408feb
Sith <227094824+python3demon@users.noreply.github.com>
2026-09-13 08:11:41 +0300

    feat: унификация вывода разных видов черновиков #10

--- src/handlers/drafts.py
+++ src/handlers/drafts.py
@@ -18,12 +18,15 @@ async def cat_draft(callback: CallbackQuery):
     num_draft = callback.data.split("_")[-1]
     post_text = config_user["drafts"][num_draft]["post"]
     group_photo = config_user["drafts"][num_draft]["group_photo"]
+    await callback.message.delete()
     
     if not group_photo:
-        await callback.message.edit_text(f"{post_text}\n\nВаш черновик*", reply_markup=None)
+        await callback.message.answer(f"{post_text}\n\nВаш черновик*")
     else:
         builder = MediaGroupBuilder(caption=post_text)
         for photo_id in group_photo:
             builder.add_photo(media=photo_id)
         
         await callback.message.answer_media_group(media=builder.build())
+    
+    await callback.answer()
