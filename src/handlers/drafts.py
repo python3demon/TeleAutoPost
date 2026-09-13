01e97c70e015a530da0b9cb1459525ae01f4d50f
Sith <227094824+python3demon@users.noreply.github.com>
2026-09-13 08:41:14 +0300

    feat: кнопка возврата к списку черновиков #10

--- src/handlers/drafts.py
+++ src/handlers/drafts.py
@@ -1,17 +1,19 @@
 from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from config import config_user
-from keyboards.inline import build_list_draft
+from keyboards.inline import build_list_draft, draft_edit
 from aiogram.utils.media_group import MediaGroupBuilder
 
 router = Router()
 
-@router.message(F.text == "Мои черновики")
-async def get_my_draft(message: Message):
+async def send_drafts(message: Message):
     posts = config_user["drafts"]
-
     buttons_markup = await build_list_draft(posts)
     await message.answer("Ваши черновики", reply_markup=buttons_markup)
+
+@router.message(F.text == "Мои черновики")
+async def get_my_draft(message: Message):
+    await send_drafts(message)
 
 @router.callback_query(F.data.startswith("view_"))
 async def cat_draft(callback: CallbackQuery):
@@ -28,5 +30,14 @@ async def cat_draft(callback: CallbackQuery):
             builder.add_photo(media=photo_id)
         
         await callback.message.answer_media_group(media=builder.build())
+    
+    await callback.message.answer(f"Ваши действия?", reply_markup=draft_edit)
+    
+    await callback.answer()
+
+@router.callback_query(F.data == "back")
+async def get_back_drafts(callback: CallbackQuery):
+    await callback.message.delete()
+    await send_drafts(callback.message)
     
     await callback.answer()
--- src/keyboards/inline.py
+++ src/keyboards/inline.py
@@ -18,3 +18,7 @@ async def build_list_draft(drafts: dict):
     post_buttons.adjust(1)
 
     return post_buttons.as_markup()
+
+draft_edit = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Назад", callback_data="back")]
+])
